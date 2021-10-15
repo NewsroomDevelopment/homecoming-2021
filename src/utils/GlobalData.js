81d408ddaf5afe35a2736606a8462df35959f11f
PatP15 <30728443+PatP15@users.noreply.github.com>
2021-10-14 21:19:08 -0400

    global data update

--- src/utils/GlobalData.js
+++ src/utils/GlobalData.js
@@ -1,172 +1,78 @@
 const GlobalData = {
-    "Senior Columns": [
-        {
-            title: "If you’re reading this, it’s late",
-            link: "https://www.columbiaspectator.com/the-eye/2021/04/29/if-youre-reading-this-its-late/",
-            author: "Isabel Jauregui",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/5AJHA7YTUNEJLOO4XNLCT7NPEE.jpg"
-        },
-        {
-            title: "Lessons From a Virtual Charli XCX Concert",
-            link: "https://www.columbiaspectator.com/the-eye/2021/04/29/lessons-from-a-virtual-charli-xcx-concert/",
-            author: "Eden Teferi",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/FCDA6CQQ6VAPVIU7GM7B5JAJ3Q.png"
-        },
-        {
-            title: "I Got a Dog (Among Other Things)",
-            link: "https://www.columbiaspectator.com/the-eye/2021/04/29/i-got-a-dog-among-other-things/",
-            author: "Candy Chan",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/WUORGBBP3FEIVAS6Z44TE5RY7M.jpg"
-        },
-        {
-            title: "Catching the Columbia Blues",
-            link: "https://www.columbiaspectator.com/the-eye/2021/04/29/catching-the-columbia-blues/",
-            author: "Jazilah Salam",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/MHAIMZ4QKNG4DIWXEKPG4R4YT4.jpg"
-        },
-        {
-            title: "AJ McDougall Is Not A Senior",
-            link: "https://www.columbiaspectator.com/the-eye/2021/04/29/senior-column-aj-mcdougall-is-not-a-senior/",
-            author: "AJ McDougall",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/V5WDP5ZV7NCUJAFSJQ2YNHAN3Q.jpg"
-        },
-        {
-            title: "When there was Columbia blue",
-            link: "https://www.columbiaspectator.com/opinion/2021/04/13/when-there-was-columbia-blue/",
-            author: "Jacob Mazzarella",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/N2Y4KDILS5D2POYNYPHN4X6J34.jpg"
-        },
-    ],
-    "News": [
-        {
-            title: "Class day speaker, valedictorian, and salutatorian announced for School of General Studies",
-            link: "https://www.columbiaspectator.com/news/2021/03/31/class-day-speaker-valedictorian-and-salutatorian-announced-for-school-of-general-studies/",
-            author: "Stella Pagkas",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/NA5QZKGUSJFIFMDKR6QP2POAEQ.jpg"
-        },
-        {
-            title: "Virtual Commencement is made bittersweet with the pandemic coming under control",
-            link: "https://www.columbiaspectator.com/news/2021/04/29/virtual-commencement-is-made-bittersweet-with-the-pandemic-coming-under-control/",
-            author: "Talia Abrahamson",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/DVTQQRVHMVEGHCO7RWXPOYP2NM.jpg"
-        },
-        {
-            title: "Inaugural cohort of Displaced Student Scholars sees its first graduates this year",
-            link: "https://www.columbiaspectator.com/news/2021/04/29/inaugural-cohort-of-displaced-student-scholars-sees-its-first-graduates-this-year/",
-            author: "Ashley Yung",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/IMEFZJVYWFHD5PXCJLMNCNLKOA.jpg"
-        },
-        {
-            title: "Joon Baek, CC ’21, leaves a legacy of international student advocacy",
-            link: "https://www.columbiaspectator.com/news/2021/04/29/joon-baek-cc-21-leaves-a-legacy-of-international-student-advocacy/",
-            author: "Abby Melbourne",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ZFJ7J6YEZRBVHFWN54T7FUJYDE.jpg"
-        },
-        {
-            title: "Senior thesis ‘A Protest, Ongoing’ explores a tradition of student activism",
-            link: "https://www.columbiaspectator.com/news/2021/04/29/senior-thesis-a-protest-ongoing-explores-a-tradition-of-student-activism/",
-            author: "Irie Sentner and Stella Pagkas",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/TOIQZUR2QFG6NM46OOLKMR5TSI.jpg"
-        },
-        {
-            title: "Anjali Verma, SEAS ’21, advocated for marginalized students in STEM and across the University",
-            link: "https://www.columbiaspectator.com/news/2021/04/29/anjali-verma-seas-21-advocated-for-marginalized-students-in-stem-and-across-the-university/",
-            author: "Ashley Yung",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/OEZ7DWX7SZF75IHX5QFPLYUA2A.jpg"
-        },
-        {
-            title: "Ceon White, GS ’21: ‘Just keep pushing on’",
-            link: "https://www.columbiaspectator.com/news/2021/04/28/ceon-white-gs-21-just-keep-pushing-on/",
-            author: "Maya Mitrasinovic",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/QLZI4VUGDVDCTKQQBHBRRU5LGU.jpg"
-        },
-    ],
     "Sports": [
         {
-            title: "After 42 years, Columbia trainer Jim Gossett retires",
-            link: "https://www.columbiaspectator.com/main/2021/04/27/after-42-years-columbia-trainer-jim-gossett-retires/",
-            author: "Wick Hallos",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/W6EGDVNQCBAHJCOPNLJ5O367GA.gif"
+            title: "Homecoming Preview",
+            link: "https://www.columbiaspectator.com/sports/2019/10/19/football-defeats-penn-44-6-in-largest-homecoming-victory-in-programs-149-year-history/",
+            author: "Harris Walker and Elizabeth Karpen",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/JV3NGZQ75ZADBLVIA6PW65WIRU.jpg"
         },
         {
-            title: "‘His footprint is all over the program’: How Josh Wainwright broke records and spearheaded the revitalization of Columbia football",
-            link: "https://www.columbiaspectator.com/sports/2021/04/28/his-footprint-is-all-over-the-program-how-josh-wainwright-broke-records-and-spearheaded-the-revitalization-of-columbia-football/",
-            author: "Bernard Wang",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JO2PEX7OCZGFZKSDTXKWAZLALA.jpg"
+            title: "Giving Day",
+            link: "https://www.columbiaspectator.com/sports/2019/10/19/football-defeats-penn-44-6-in-largest-homecoming-victory-in-programs-149-year-history/",
+            author: "Harris Walker and Elizabeth Karpen",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/JV3NGZQ75ZADBLVIA6PW65WIRU.jpg"
         },
         {
-            title: "Josh Nicoloff leads Columbia baseball by example, even after losing junior and senior seasons",
-            link: "https://www.columbiaspectator.com/main/2021/04/29/how-baseball-improvised-to-stay-together-and-improve-throughout-the-pandemic/",
-            author: "Robert Gao",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/L4M26QZYBBBRBKC5AE24EW2YEQ.jpg"
+            title: "1961 Matt's Huge Piece",
+            link: "https://www.columbiaspectator.com/sports/2019/10/19/football-defeats-penn-44-6-in-largest-homecoming-victory-in-programs-149-year-history/",
+            author: "Harris Walker and Elizabeth Karpen",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/JV3NGZQ75ZADBLVIA6PW65WIRU.jpg"
         },
         {
-            title: "How baseball improvised to stay together and improve throughout the pandemic",
-            link: "https://www.columbiaspectator.com/sports/2021/04/28/josh-nicoloff-leads-columbia-baseball-by-example-even-after-losing-junior-and-senior-seasons/",
-            author: "Miles Schachner and Rebecca Wachen",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/U2ICLIGXYZDVNDQOBJZBUJDZ3E.gif"
+            title: "Football defeats Penn 44-6 in largest Homecoming victory in program’s 149-year history",
+            link: "https://www.columbiaspectator.com/sports/2019/10/19/football-defeats-penn-44-6-in-largest-homecoming-victory-in-programs-149-year-history/",
+            author: "Harris Walker and Elizabeth Karpen",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/JV3NGZQ75ZADBLVIA6PW65WIRU.jpg"
         },
         {
-            title: "Men’s basketball seniors reflect on overcoming adversity and the road ahead",
-            link: "https://www.columbiaspectator.com/main/2021/04/29/mens-basketball-seniors-reflect-on-overcoming-adversity-and-the-road-ahead/",
-            author: "Miles Schachner and Matthew Kim",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/LHKTYLAU5NDLVLTS5E6MWKAUZ4.jpg"
-        }
+            title: "Columbia football is back. These are the players to watch this season",
+            link: "https://www.columbiaspectator.com/the-eye/2021/04/29/lessons-from-a-virtual-charli-xcx-concert/",
+            author: "Matthew Kim",
+            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/3XQQKHA3TBDS5ATFGCVRWZMHOI.jpg"
+        },
+        {
+            title: "How to do Homecoming if you're actually excited about it",
+            link: "https://www.columbiaspectator.com/spectrum/2016/10/20/your-homecoming-weekend-itinerary-whats-wheres-and-hows/",
+            author: "Stephanie Sloves",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/HFQ26I4J45DR5HHTJLO5WLW4IU"
+        },
+        {
+            title: "Amid challenges, assistant coaches drive every athlete forward",
+            link: "https://www.columbiaspectator.com/sports/2020/10/27/amid-challenges-assistant-coaches-drive-every-athlete-forward/",
+            author: "Matthew Kim",
+            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/XD2TFZY2CRGS5N5Q3A5JS6PQYY.jpg"
+        },
+        {
+            title: "Remembering Tyler Ugolyn, CC ’01, 20 years later",
+            link: "https://www.columbiaspectator.com/main/2021/09/14/remembering-tyler-ugolyn-cc-01-20-years-later/",
+            author: "Miles Schachner",
+            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ADQXAT5DNBBQBKKNK3SCEKV4WA.jpg"
+        },
+        {
+            title: "Joe Green",
+            link: "https://www.columbiaspectator.com/sports/2019/10/19/football-defeats-penn-44-6-in-largest-homecoming-victory-in-programs-149-year-history/",
+            author: "Harris Walker and Elizabeth Karpen",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/JV3NGZQ75ZADBLVIA6PW65WIRU.jpg"
+        },
     ],
-    "Arts and Entertainment": [
+    "Opinion": [
         {
-            title: "Barnard theater senior thesis festival makes a dazzling return to the Glicker-Milstein Theatre",
-            link: "https://www.columbiaspectator.com/arts-and-entertainment/2021/04/13/barnard-theater-senior-thesis-festival-makes-a-dazzling-return-to-the-glicker-milstein-theatre/",
-            author: "Sophie Craig",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/2DYHDEZWCFFCLFP75VEYW32VAE.jpg"
+            title: "Columbia does not deserve alumni donations",
+            link: "https://www.columbiaspectator.com/opinion/2021/04/01/columbia-does-not-deserve-alumni-donations/",
+            author: "Joel Berg",
+            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VY5OFVDG5JAN5NCPLBYPGJEGWU.jpg"
         },
         {
-            title: "‘We all are roses’: Mamadou., CC ’21, discusses faith and vulnerability in his debut album",
-            link: "https://www.columbiaspectator.com/arts-and-entertainment/2021/01/21/we-all-are-roses-mamadou-cc-21-discusses-faith-and-vulnerability-in-his-debut-album/",
-            author: "Esterah Brown",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/CI2G54KHKRA5TJDI5TM55V5O3I.jpg"
+            title: "Columbia State University for a weekend",
+            link: "https://www.columbiaspectator.com/opinion/2015/10/22/columbia-state-university-weekend/",
+            author: "Angela Beam",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/JJTYHDO5Q5H4HBDIB4QP4WK4TQ"
         },
         {
-            title: "New student-led journal That Which Remains sparks important conversations about justice",
-            link: "https://www.columbiaspectator.com/arts-and-entertainment/2021/04/08/new-student-led-journal-that-which-remains-sparks-important-conversations-about-justice/",
-            author: "Clare Donaldson",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/YDOUXQLNPVEMPBVTLAEGC7OMMU.JPG"
-        },
-        {
-            title: "Musician Grace Victoria D’Haiti, BC ’21, uses her voice to advocate and educate",
-            link: "https://www.columbiaspectator.com/arts-and-entertainment/2021/02/10/musician-grace-victoria-dhaiti-bc-21-uses-her-voice-to-advocate-and-educate/",
-            author: "Esterah Brown",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/AMEHVVELDRDTZA54IXVNAN4DXE.jpg"
-        },
-        {
-            title: "Behind the curtain: Mario Alberto Garcia Jr., CC ’21, on Columbia’s diverse and growing theater community",
-            link: "https://www.columbiaspectator.com/arts-and-entertainment/2021/04/27/behind-the-curtain-mario-alberto-garcia-jr-cc-21-on-columbias-diverse-and-growing-theater-community/",
-            author: "Irene Madrigal",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/DEWC6YHASRGPJGVDRJHV453MYE.JPG"
-        },
-        {
-            title: "CU Generation Virtual Showcase highlights Asian American cultural diversity with ‘GENESIS’",
-            link: "https://www.columbiaspectator.com/arts-and-entertainment/2021/04/27/cu-generation-virtual-showcase-highlights-asian-american-cultural-diversity-with-genesis/",
-            author: "Liz Radway",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/XFMJ4XYHQNFOROSJIML6KTYLHQ.jpeg"
-        },
-        {
-            title: "Poet Asha Futterman, BC ’21, on theater, walking tours, and Lake Michigan",
-            link: "https://www.columbiaspectator.com/arts-and-entertainment/2021/04/28/poet-asha-futterman-bc-21-on-theater-walking-tours-and-lake-michigan/",
-            author: "Sophie Craig",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/YD6LBHZ2KZAXFJA5H6KJCE25X4.jpeg"
-        },
-        {
-            title: "Soorim Lee, CC ’21, on her joyous journey through the Columbia film world",
-            link: "https://www.columbiaspectator.com/arts-and-entertainment/2021/04/27/soorim-lee-cc-21-on-her-joyous-journey-through-the-columbia-film-world/",
-            author: "Bella Druckman",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/NWPECMVCQRDC7DYNYW3T5DYO3E.jpg"
-        },
-        {
-            title: "Seniors reflect on their time as part of Columbia’s dance community",
-            link: "https://www.columbiaspectator.com/arts-and-entertainment/2021/04/28/seniors-reflect-on-their-time-as-part-of-columbias-dance-community/",
-            author: "Liz Radway and Emma Danon",
-            img: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/6UKPRDAIDVCFLDNJVDBLMKTGGI.JPG"
+            title: "The stories that matter",
+            link: "https://www.columbiaspectator.com/opinion/2015/10/15/stories-matter/",
+            author: "Colin Valentini",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/RZU7SDXT45BKRMCNLMPOKJFTVE"
         }
     ]
 }
