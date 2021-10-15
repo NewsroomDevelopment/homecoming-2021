b278dd13669df1cc19572bc492679d4fd6fe869a
HongSenDu <hd2455@columbia.edu>
2021-10-15 17:26:29 -0400

    Updates

---
+++ precache-manifest.c87b76066a7be425df937bd81ef66139.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9244f9b641b00530fff2573a7412fab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "417900bf536aa1db571d",
+    "url": "/static/css/2.b871a977.chunk.css"
+  },
+  {
+    "revision": "3727ad2dbbcdc98b8ad3",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "417900bf536aa1db571d",
+    "url": "/static/js/2.e8f2054a.chunk.js"
+  },
+  {
+    "revision": "3727ad2dbbcdc98b8ad3",
+    "url": "/static/js/main.6793870d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "63bf78e3c5073827f8ce863b23e3da8f",
+    "url": "/static/media/background.63bf78e3.png"
+  },
+  {
+    "revision": "0b82b3e3d8421d1f408a4cbdbb262417",
+    "url": "/static/media/background1.0b82b3e3.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
